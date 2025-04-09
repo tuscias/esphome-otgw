04666cf4d63d702c8bb499d0197544104e9aea23
Geert <geert@kroone.net>
2025-04-09 21:37:02 +0200

    add Relative Modulation level, Max rel mod level and update README.md to include new sensors.

--- components/otgw/sensor/__init__.py
+++ components/otgw/sensor/__init__.py
@@ -29,7 +29,8 @@ SENSOR_ROOM_TEMPERATURE = "room_temperature"
 SENSOR_BOILER_WATER_TEMPERATURE = "boiler_water_temperature"
 SENSOR_BURNER_STARTS = "burner_starts"
 SENSOR_BURNER_OPERATION_HOURS = "burner_operation_hours"
-
+SENSOR_MAX_RELATIVE_MODULATION_LEVEL = "max_relative_modulation_level"
+SENSOR_RELATIVE_MODULATION_LEVEL = "relative_modulation_level"
 
 @dataclass
 class OpenThermGatewaySensorConfig:
@@ -45,6 +46,10 @@ SENSOR_CONFIG = {
     SENSOR_BOILER_WATER_TEMPERATURE: OpenThermGatewaySensorConfig(25, "F88"),
     SENSOR_BURNER_STARTS: OpenThermGatewaySensorConfig(116, "U16", clear_on_timeout=False),
     SENSOR_BURNER_OPERATION_HOURS: OpenThermGatewaySensorConfig(120, "U16", clear_on_timeout=False),
+    SENSOR_CONTROL_SETPOINT: OpenThermGatewaySensorConfig(1, "F88"),
+    SENSOR_ROOM_SETPOINT: OpenThermGatewaySensorConfig(16, "F88"),
+    SENSOR_MAX_RELATIVE_MODULATION_LEVEL: OpenThermGatewaySensorConfig(14, "F88"),
+    SENSOR_RELATIVE_MODULATION_LEVEL: OpenThermGatewaySensorConfig(17, "F88"),
 }
 
 
@@ -55,6 +60,18 @@ CONFIG_SCHEMA = cv.Schema({
         unit_of_measurement=UNIT_CELSIUS,
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_TEMPERATURE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(SENSOR_MAX_RELATIVE_MODULATION_LEVEL): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement='%',
+        accuracy_decimals=2,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(SENSOR_RELATIVE_MODULATION_LEVEL): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement='%',
+        accuracy_decimals=2,
         state_class=STATE_CLASS_MEASUREMENT,
     ),
     cv.Optional(SENSOR_ROOM_SETPOINT): sensor.sensor_schema(
