71d29437923ddfc7f3b57d5ec0f9568a73c7c2fe
tuscias <tomas.sakinis@gmail.com>
2025-10-19 18:29:33 +0300

    Add new sensor constants and configurations

--- components/otgw/sensor/__init__.py
+++ components/otgw/sensor/__init__.py
@@ -32,6 +32,21 @@ SENSOR_BURNER_STARTS = "burner_starts"
 SENSOR_BURNER_OPERATION_HOURS = "burner_operation_hours"
 SENSOR_MAX_RELATIVE_MODULATION_LEVEL = "max_relative_modulation_level"
 SENSOR_RELATIVE_MODULATION_LEVEL = "relative_modulation_level"
+SENSOR_DHW_TEMPERATURE = "dhw_temperature"
+SENSOR_OUTSIDE_TEMPERATURE = "outside_temperature"
+SENSOR_RETURN_WATER_TEMPERATURE = "return_water_temperature"
+SENSOR_DHW_FLOW_RATE = "dhw_flow_rate"
+SENSOR_CH2_FLOW_TEMPERATURE = "ch2_flow_temperature"
+SENSOR_CONTROL_SETPOINT_2 = "control_setpoint_2"
+SENSOR_EXHAUST_TEMPERATURE = "exhaust_temperature"
+SENSOR_CH_PUMP_STARTS = "ch_pump_starts"
+SENSOR_DHW_PUMP_VALVE_STARTS = "dhw_pump_valve_starts"
+SENSOR_DHW_BURNER_STARTS = "dhw_burner_starts"
+SENSOR_CH_PUMP_OPERATION_HOURS = "ch_pump_operation_hours"
+SENSOR_DHW_PUMP_VALVE_OPERATION_HOURS = "dhw_pump_valve_operation_hours"
+SENSOR_DHW_BURNER_OPERATION_HOURS = "dhw_burner_operation_hours"
+SENSOR_OPENTHERM_VERSION_MASTER = "opentherm_version_master"
+SENSOR_OPENTHERM_VERSION_SLAVE = "opentherm_version_slave"
 
 @dataclass
 class OpenThermGatewaySensorConfig:
@@ -49,6 +64,21 @@ SENSOR_CONFIG = {
     SENSOR_BOILER_WATER_TEMPERATURE: OpenThermGatewaySensorConfig(25, "F88"),
     SENSOR_BURNER_STARTS: OpenThermGatewaySensorConfig(116, "U16", clear_on_timeout=False),
     SENSOR_BURNER_OPERATION_HOURS: OpenThermGatewaySensorConfig(120, "U16", clear_on_timeout=False),
+    SENSOR_DHW_TEMPERATURE: OpenThermGatewaySensorConfig(26, "F88"),
+    SENSOR_OUTSIDE_TEMPERATURE: OpenThermGatewaySensorConfig(27, "F88"),
+    SENSOR_RETURN_WATER_TEMPERATURE: OpenThermGatewaySensorConfig(28, "F88"),
+    SENSOR_DHW_FLOW_RATE: OpenThermGatewaySensorConfig(19, "F88"),
+    SENSOR_CH2_FLOW_TEMPERATURE: OpenThermGatewaySensorConfig(31, "F88"),
+    SENSOR_CONTROL_SETPOINT_2: OpenThermGatewaySensorConfig(8, "F88"),
+    SENSOR_EXHAUST_TEMPERATURE: OpenThermGatewaySensorConfig(33, "S16"),
+    SENSOR_CH_PUMP_STARTS: OpenThermGatewaySensorConfig(117, "U16", clear_on_timeout=False),
+    SENSOR_DHW_PUMP_VALVE_STARTS: OpenThermGatewaySensorConfig(118, "U16", clear_on_timeout=False),
+    SENSOR_DHW_BURNER_STARTS: OpenThermGatewaySensorConfig(119, "U16", clear_on_timeout=False),
+    SENSOR_CH_PUMP_OPERATION_HOURS: OpenThermGatewaySensorConfig(121, "U16", clear_on_timeout=False),
+    SENSOR_DHW_PUMP_VALVE_OPERATION_HOURS: OpenThermGatewaySensorConfig(122, "U16", clear_on_timeout=False),
+    SENSOR_DHW_BURNER_OPERATION_HOURS: OpenThermGatewaySensorConfig(123, "U16", clear_on_timeout=False),
+    SENSOR_OPENTHERM_VERSION_MASTER: OpenThermGatewaySensorConfig(124, "F88"),
+    SENSOR_OPENTHERM_VERSION_SLAVE: OpenThermGatewaySensorConfig(125, "F88"),
 }
 
 
@@ -60,6 +90,107 @@ CONFIG_SCHEMA = cv.Schema({
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_TEMPERATURE,
         state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(SENSOR_DHW_TEMPERATURE): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(SENSOR_OUTSIDE_TEMPERATURE): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(SENSOR_RETURN_WATER_TEMPERATURE): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(SENSOR_DHW_FLOW_RATE): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement='l/min',
+        accuracy_decimals=2,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(SENSOR_CH2_FLOW_TEMPERATURE): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(SENSOR_CONTROL_SETPOINT_2): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(SENSOR_EXHAUST_TEMPERATURE): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(SENSOR_CH_PUMP_STARTS): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_EMPTY,
+        accuracy_decimals=0,
+        state_class=STATE_CLASS_TOTAL_INCREASING,
+    ),
+    cv.Optional(SENSOR_DHW_PUMP_VALVE_STARTS): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_EMPTY,
+        accuracy_decimals=0,
+        state_class=STATE_CLASS_TOTAL_INCREASING,
+    ),
+    cv.Optional(SENSOR_DHW_BURNER_STARTS): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_EMPTY,
+        accuracy_decimals=0,
+        state_class=STATE_CLASS_TOTAL_INCREASING,
+    ),
+    cv.Optional(SENSOR_CH_PUMP_OPERATION_HOURS): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_HOUR,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_DURATION,
+        state_class=STATE_CLASS_TOTAL_INCREASING,
+    ),
+    cv.Optional(SENSOR_DHW_PUMP_VALVE_OPERATION_HOURS): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_HOUR,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_DURATION,
+        state_class=STATE_CLASS_TOTAL_INCREASING,
+    ),
+    cv.Optional(SENSOR_DHW_BURNER_OPERATION_HOURS): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_HOUR,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_DURATION,
+        state_class=STATE_CLASS_TOTAL_INCREASING,
+    ),
+    cv.Optional(SENSOR_OPENTHERM_VERSION_MASTER): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_EMPTY,
+        accuracy_decimals=2,
+        state_class=STATE_CLASS_MEASUREMENT,
+        entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+    ),
+    cv.Optional(SENSOR_OPENTHERM_VERSION_SLAVE): sensor.sensor_schema(
+        OpenThermGatewaySensor,
+        unit_of_measurement=UNIT_EMPTY,
+        accuracy_decimals=2,
+        state_class=STATE_CLASS_MEASUREMENT,
+        entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
     ),
     cv.Optional(SENSOR_MAX_RELATIVE_MODULATION_LEVEL): sensor.sensor_schema(
         OpenThermGatewaySensor,
